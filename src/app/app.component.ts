d322af184a9c6d7e2a22e78eff89488ffb6f7146
juaneins_uio <ballesterosjg@gmail.com>
2022-06-17 12:11:29 -0500

    angular life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,35 +8,14 @@ import { Product } from './model/product.model';
 })
 export class AppComponent {
   title = 'angular-store-2022';
-  imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpeg',
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpeg',
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumes',
-      price: 34,
-      image: './assets/images/album.jpeg',
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpeg',
-    },
-  ];
+  imgParent = '';
+  showImage = true;
 
   onLoaded(img: string) {
     console.log('log padre', img);
+  }
+
+  toggleImage() {
+    this.showImage = !this.showImage;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,70 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
 })
-export class ImgComponent implements OnInit {
-  @Input() img: string = '';
+export class ImgComponent
+  implements OnInit, OnChanges, AfterViewInit, OnDestroy
+{
+  img: string = '';
+  @Input('img') set changeImg(newImage: string) {
+    this.img = newImage;
+    console.log('Change just img =>', this.img);
+
+    //code here!
+  }
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/hackerman.jpeg';
+  counter = 0;
+  counterFn: number | undefined;
 
-  constructor() {}
+  constructor() {
+    // before render
+    // no async --run one time
 
-  ngOnInit(): void {}
+    console.log('img constructor ', 'imgValue=> ', this.img);
+  }
+  ngOnDestroy(): void {
+    // run when delete the component
+    console.log('ngOnDestroy', 'imgValue=> ', this.img);
+    window.clearInterval(this.counterFn);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // before, during render
+    // inputs changes -- runs many times
+    console.log('ngOnChanges ', 'imgValue=> ', this.img);
+    console.log('changes', changes);
+  }
+
+  ngOnInit(): void {
+    // before render
+    // async - fetch -- runs one time
+    console.log('ngOnInit ', 'imgValue=> ', this.img);
+    this.counterFn = window.setInterval(() => {
+      this.counter++;
+      console.log('run counter');
+    }, 1000);
+  }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children
+
+    console.log('ngAfterViewInit ', 'imgValue=> ', this.img);
+  }
 
   imgError() {
     this.img = this.imageDefault;
