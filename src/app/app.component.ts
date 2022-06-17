aabdaeb9f8b44e94e4335f1c693abf040d236a51
juaneins_uio <ballesterosjg@gmail.com>
2022-06-16 19:10:40 -0500

    create product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './model/product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-store-2022';
   imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpeg',
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpeg',
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumes',
+      price: 34,
+      image: './assets/images/album.jpeg',
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpeg',
+    },
+  ];
 
   onLoaded(img: string) {
     console.log('log padre', img);
